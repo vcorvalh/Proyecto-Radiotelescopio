2bff776e37cc31f34832bddab738e0bba86fd30e
Vicente Corvalan <vcorvalh@uc.cl>
2023-03-30 21:02:31 -0300

    signal_receiver.py

--- signal_test.py
+++ signal_test.py
@@ -4,14 +4,14 @@ from matplotlib.animation import FuncAnimation
 from rtlsdr import RtlSdr
 
 # Frecuencia central y ancho de banda de la señal de interés (en Hz)
-center_freq = 1e9   # Frecuencia del LNB
-bandwidth = 5e6   # Ancho de banda de 5 MHz
+center_freq = 1.42e9   # Frecuencia del LNB
+bandwidth = 100e6   # Ancho de banda de 5 MHz
 
 # Configuración del dongle
 sdr = RtlSdr()
 sdr.rs = 2.4e6   # Frecuencia de muestreo (en Hz)
 sdr.fc = center_freq
-sdr.gain = 49.6
+sdr.gain = 1
 sdr.bandwidth = bandwidth
 
 # Inicialización del gráfico
@@ -22,13 +22,16 @@ ax.set_ylabel('Potencia (dB)')
 ax.set_title('Espectro de frecuencia')
 ax.set_ylim(bottom=-40, top=70)
 
+
+
 # Función que se ejecuta en cada frame de la animación
 def update(frame):
     # Captura de datos
-    samples = sdr.read_samples(512*1024)  # Número de muestras
+    inputs = sdr.read_samples(512*1024)
+    samples = np.absolute(inputs)  # Número de muestras
     fft_data = np.fft.fft(samples)
     freqs = np.fft.fftfreq(len(samples), 1/sdr.sample_rate)
-    power_spectrum = 20*np.log10(np.abs(fft_data))
+    power_spectrum = 10*np.log10(np.abs(fft_data))
 
     # Actualización de los datos del gráfico
     line.set_xdata(freqs)
--- time_signal.py
+++ time_signal.py
@@ -4,35 +4,37 @@ from matplotlib.animation import FuncAnimation
 from rtlsdr import RtlSdr
 
 # Frecuencia central y ancho de banda de la señal de interés (en Hz)
-center_freq = 1e9   # Frecuencia del LNB
-bandwidth = 5e6   # Ancho de banda de 5 MHz
+center_freq = 1.42e9   # Frecuencia del LNB
+bandwidth = 100e6   # Ancho de banda de 5 MHz
 
 # Configuración del dongle
 sdr = RtlSdr()
 sdr.rs = 2.4e6   # Frecuencia de muestreo (en Hz)
 sdr.fc = center_freq
-sdr.gain = 49.6
+sdr.gain = 100
 sdr.bandwidth = bandwidth
 
 # Inicialización del gráfico
 fig, ax = plt.subplots()
 line, = ax.plot([], [])
-ax.set_xlabel('Frecuencia (Hz)')
-ax.set_ylabel('Potencia (dB)')
+ax.set_xlabel('Tiempo (s)')
+ax.set_ylabel('Amplitud (V)')
 ax.set_title('Espectro de frecuencia')
-ax.set_ylim(bottom=-40, top=70)
+ax.set_ylim(bottom=-1, top=1)
+
+inputs = sdr.read_samples(10)
+print(inputs)
 
 # Función que se ejecuta en cada frame de la animación
 def update(frame):
     # Captura de datos
-    samples = sdr.read_samples(512*1024)  # Número de muestras
+    samples = sdr.read_samples(256*1024)  # Número de muestras
     fft_data = np.fft.fft(samples)
-    freqs = np.fft.fftfreq(len(samples), 1/sdr.sample_rate)
-    power_spectrum = 20*np.log10(np.abs(fft_data))
+    real_data = np.fft.ifft(fft_data)
 
     # Actualización de los datos del gráfico
-    line.set_xdata(freqs)
-    line.set_ydata(power_spectrum)
+    line.set_xdata(np.arange(len(real_data))/sdr.rs)
+    line.set_ydata(real_data)
     ax.relim()
     ax.autoscale_view()
 
